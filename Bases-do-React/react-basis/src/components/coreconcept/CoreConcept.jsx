59077e603ac2dcfcd993214d3623971335e79bee
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2024-12-02 22:11:11 -0300

    falta corrigir

---
+++ Bases-do-React/react-basis/src/components/coreconcept/CoreConcept.jsx
@@ -0,0 +1,49 @@
+import './CoreConcept.css';
+
+export const conceitosChaves = [
+  {
+    id: 1,
+    titulo: 'Componentes',
+    conceitos: [
+      'Componentes são pequenos blocos de código que podem ser reutilizados em vários lugares da interface.',
+      'Os componentes são imutáveis, ou seja, não podem ser alterados diretamente.',
+      'Os componentes podem ser passados props para outros componentes.',
+      'Os componentes podem ser estilizados usando CSS ou CSS-in-JS.',
+    ]
+  },
+  {
+    id: 2,
+    titulo: 'Estados e eventos',
+    conceitos: [
+      'Os estados são dados que são mantidos pelo componente.',
+      'Os estados são imutáveis, ou seja, não podem ser alterados diretamente.',
+      'Os estados são usados para controlar o estado do componente.',
+      'Os estados são usados para gerenciar o estado do componente, além de lidar com eventos.',
+    ],
+  },
+  {
+    id:3,
+    titulo: 'Props',
+    conceitos: [
+      'Props são dados que são passados de um componente pai para um componente filho.',
+      'Os props são imutáveis, ou seja, não podem ser alterados diretamente.',
+      'Os props são usados para passar dados entre componentes.',
+      'Os props são usados para passar dados entre componentes, além de lidar com eventos.',
+    ]
+  }
+];
+const CoreConcepts = ({titulo, conceitos}) => {
+    return (
+      <>
+      <h2>{titulo}</h2>
+      <ul id="concept-list">
+         {conceitos.map((item, key) => {
+          return <li key={key}>{item}</li>;
+         })}
+        </ul>
+      
+      </>
+    )
+  };
+
+export default CoreConcepts;
--- Bases-do-React/react-basis/src/components/main/Main.jsx
+++ Bases-do-React/react-basis/src/components/main/Main.jsx
@@ -1,22 +1,26 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import CoreConcepts, { conceitosChaves } from '../coreconcept/CoreConcept.jsx';
 import './Main.css';
 import MeuObjetivoComReact from './meuObjetivoReact/MeuObjetivoComReact';
 
-const CoreConcepts = () =>{
-    return (
-        <>
-        <h2>Conceitos chaves</h2>
-        <ul>
-            <li>React é uma biblioteca JavaScript para construir interfaces de usuário.</li>
-        </ul>
-        </>
-    )
-}
+// Atividade:
+// 1- Passe o componente 'CoreConcept' para um novo arquivo e pasta tendo como base o que foi ensinado sobre estruturação de componentes e arquivos no React.
+// 2- Determine uma estilização específica para que o conteúdo em 'CoreConcept' não ultrapasse a largura de 32rem e que, epecificamente, o texto seja alinhado à esquerda e com até 28rem de largura;
+// 3- Passe as props 'id', 'titulo', 'conceitos' para 'CoreConcept'
+// 4- Renderize CoreConcept tendo como base o array conceitosChaves passando cada valor necessário para suas props.
+// 5- Tire print de sua solução e envie o commit como entrega da atividade
 
 const Main = () => {
     return (
       <main>
         <MeuObjetivoComReact />
-        <section></section>
+        <section id='core-concepts'>
+          conceitosChaves.map((conceitosChave,key) => {
+            return <CoreConcepts key={key} titulo={conceitoChave.titulo}  conceitos={conceitosChaves[0].conceitos}/>
+          })
+        
+        </section>
       </main>
     );
   };
