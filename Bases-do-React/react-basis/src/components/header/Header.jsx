dd04b20325bab004d18e92a5bace83d7bfeaba29
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2024-12-03 22:08:58 -0300

    Aula de 3/12

--- Bases-do-React/react-basis/src/components/header/Header.jsx
+++ Bases-do-React/react-basis/src/components/header/Header.jsx
@@ -1,22 +1,25 @@
-import Image from '../../assets/ab67706c0000da84302872ea0b7d69e6b7c7c40d.jfif' 
-import './Header.css'
 
-const randomWords = () =>{
-  const words = ['conceitos','fundamentos', 'definições']
-  const indice = Math.floor(Math.random() * words.length)
-  return words[indice]
-}
+/* eslint-disable no-unused-vars */
 
-
+import "./Header.css";
+import messiCareca from "../../assets/ab67706c0000da84302872ea0b7d69e6b7c7c40d.jfif";
+const randomWords = () => {
+  const words = ["conceitos", "fundamentos", "definições"];
+  const indice = Math.floor(Math.random() * words.length);
+  return words[indice];
+};
 
 const Header = () => {
-    return (
-      <header>
-        <img src={Image} alt='' />
-        <h1>Fundamentos do React</h1>
-        <p>Iremos aplicar aqui {randomWords()} práticos e resumidos do React.</p>
-      </header>
-    );
-  };
-  
+  const imgSrc = messiCareca;
+
+  return (
+    <header>
+      {/* {imgSrc ? (<img src={imgSrc} alt="" />): (<p>Imagem não encontrada</p>)} */}
+      {imgSrc && <img src={imgSrc}/>}
+      <h1>Fundamentos do React</h1>
+      <p>Iremos aplicar aqui {randomWords()} práticos e resumidos do React.</p>
+    </header>
+  );
+};
+
 export default Header;
--- Bases-do-React/react-basis/src/components/main/Main.jsx
+++ Bases-do-React/react-basis/src/components/main/Main.jsx
@@ -1,28 +1,22 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import CoreConcepts, { conceitosChaves } from '../coreconcept/CoreConcept.jsx';
-import './Main.css';
-import MeuObjetivoComReact from './meuObjetivoReact/MeuObjetivoComReact';
+import CoreConcept from "../coreconcept/CoreConcept.jsx";
+import "./Main.css";
+import MeuObjetivoComReact from "./meuObjetivoReact/MeuObjetivoComReact.jsx";
 
-// Atividade:
-// 1- Passe o componente 'CoreConcept' para um novo arquivo e pasta tendo como base o que foi ensinado sobre estruturação de componentes e arquivos no React.
-// 2- Determine uma estilização específica para que o conteúdo em 'CoreConcept' não ultrapasse a largura de 32rem e que, epecificamente, o texto seja alinhado à esquerda e com até 28rem de largura;
-// 3- Passe as props 'id', 'titulo', 'conceitos' para 'CoreConcept'
-// 4- Renderize CoreConcept tendo como base o array conceitosChaves passando cada valor necessário para suas props.
-// 5- Tire print de sua solução e envie o commit como entrega da atividade
+import {conceitosChaves} from "../../data.js"
 
 const Main = () => {
-    return (
-      <main>
-        <MeuObjetivoComReact />
-        <section id='core-concepts'>
-          conceitosChaves.map((conceitosChave,key) => {
-            return <CoreConcepts key={key} titulo={conceitoChave.titulo}  conceitos={conceitosChaves[0].conceitos}/>
-          })
-        
-        </section>
-      </main>
-    );
-  };
+  return (
+    <main>
+      <MeuObjetivoComReact />
+      <section id="core-concepts">
+        {conceitosChaves.map((conceitoChave, key) => {
+          return <CoreConcept key={key} {...conceitoChave} />;
+        })}
+      </section>
+    </main>
+  );
+};
 
-  export default Main;
+export default Main;
